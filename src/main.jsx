90bfc589aa741ba297c74d29c21d917fd469970e
vbjawahar-MIT <vbjawahar2002@gmail.com>
2026-06-15 12:31:48 +0530

    Use HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>
+  <HashRouter>
+  <App />
+</HashRouter>
 )
